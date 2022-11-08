655b1aefa0c270a99dbc81b9c31ef37aa17d48f5
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2022-11-08 23:14:42 +0545

    Time: 3 ms (82.74%), Space: 6.3 MB (82.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -4,7 +4,7 @@ public:
         string ans;
         for(int i = 0; i<s.size();i++){
             ans.push_back(s[i]);
-            while(ans.size() && (ans.back()== s[i+1] + 32 || ans.back()==s[i+1]-32)){
+            while(ans.size() && abs(ans.back() - s[i+1])== 32){
                 ans.pop_back();
                 i++;
             }
