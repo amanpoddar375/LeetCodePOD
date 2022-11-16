bc4a6966475f3ebd04b909bdf090412942de13a4
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2022-11-16 09:40:46 +0545

    Time: 0 ms (100.00%), Space: 5.9 MB (68.38%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -7,17 +7,16 @@ public:
         long int l;
         long int h = n;
         long int m;
-        long int output = n;
         while(l<=h){
             m = (h+l)/2;
             if(isBadVersion(m)){
-                output = m;
                 h = m-1;
             }
             else{
                 l = m+1;
-        }}
-        return output;
+            }
+        }
+        return l ;
         }
 
     };
