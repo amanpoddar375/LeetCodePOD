6ce890fdd5cefa4112882edc0bcaad8a2e29ef6b
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2022-11-18 07:21:28 +0545

    Time: 8 ms (11.77%), Space: 5.8 MB (86.72%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,11 +1,12 @@
+//**** Time Complexity : O(logn)**** : As the loop variables are divided by a constant amount.
 class Solution {
 public:
     bool isUgly(int n) {
-        if(n<=0)
+        if(n<=0)  // n needs to be a positive integer if not return false
             return false;
-        std::vector<int> v = { 2,3,5 };
-        for (auto i : v)
-            while(n%i==0)
+        std::vector<int> v = { 2,3,5 };  // defining range of numbers to use in the for loop
+        for (auto i : v)   //using range-for loop
+            while(n%i==0) //dividing n to check if there are other prime factor
                 n/=i;
         return n==1;
     }
