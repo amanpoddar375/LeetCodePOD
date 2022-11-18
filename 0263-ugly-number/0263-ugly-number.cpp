6cc066fbba91b4382bb3e612044a01456ef814d8
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2022-11-18 07:08:11 +0545

    Time: 0 ms (100.00%), Space: 5.9 MB (19.97%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if(n == 0) return false;
-    
-        while(n%2 == 0) n/=2;
-        while(n%3 == 0) n/=3;
-        while(n%5 == 0) n/=5;
-    
-        return n == 1;
+        if(n<=0)
+            return false;
+        std::vector<int> v = { 2,3,5 };
+        for (auto i : v)
+            while(n%i==0)
+                n/=i;
+        return n==1;
     }
 };
