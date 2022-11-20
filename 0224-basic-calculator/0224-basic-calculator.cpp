a9dbf1f4e4750c814083ff1399a52ca6dcecd295
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2022-11-20 22:53:11 +0545

    Time: 21 ms (58.65%), Space: 7.9 MB (93.14%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int sum = 0;
+        int sign = 1;
+        int n = s.size();
+        stack<pair<int,int>>stk;
+        
+        for(int i =0; i<n;i++){
+            if(s[i] >= '0' && s[i]<= '9'){
+                int num = 0;
+                while(i<n && isdigit(s[i])){
+                    num = num *10 + (s[i] - '0');
+                    i++;
+                }
+                i--;
+                sum += (num * sign);
+                sign = 1;
+            }
+            else if(s[i]=='('){
+                stk.push({sum, sign});
+                sum = 0;
+                sign = 1;
+            }
+            else if(s[i]==')'){
+                sum = stk.top().first + (stk.top().second * sum);
+                stk.pop();
+            }
+            else if(s[i]=='-'){
+                sign = (-1 * sign);
+            }
+        }
+        return sum;
+    }
+};
