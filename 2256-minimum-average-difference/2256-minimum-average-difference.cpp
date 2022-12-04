e0ba2cf5343fef59f7c07fc2edb3c3ee3bb2b6f4
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2022-12-04 21:10:05 +0545

    Time: 170 ms (73.40%), Space: 78.3 MB (96.38%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumAverageDifference(vector<int>& nums) {
+        long totalsum = 0, currentsum = 0;
+        int n = nums.size();
+        for(int i = 0; i<n;i++){
+            totalsum += nums[i];
+        }
+        int minsum = INT_MAX;
+        int answerIndex = 0;
+        for(int i = 0; i< n; i++){
+            currentsum += nums[i];
+            long fpavg = currentsum/(i+1);
+            
+            
+            if(i == n-1){
+                if(fpavg<minsum)
+                    return n-1;
+                else
+                    break;
+            }
+            long spavg = (totalsum - currentsum)/(n-i-1);
+            if(abs(fpavg - spavg)< minsum){
+                minsum = abs(fpavg - spavg);
+                answerIndex = i;
+            }
+        }
+        return answerIndex;
+    }
+};
