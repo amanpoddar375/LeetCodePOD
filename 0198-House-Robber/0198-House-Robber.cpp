8a5ca316dfe979663a1cae3a5ab595bebfcc5f16
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2022-12-14 21:19:30 +0545

    Updated 0198-House-Robber.cpp

---
+++ 0198-House-Robber/0198-House-Robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int dp[n+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1]);
+        }
+        return dp[n];
+    }
+};
