714119db05766cb6d719a5de08f137d0dd0abfe7
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2023-01-01 22:09:16 +0545

    Update 2520-Count-the-Digits-That-Divide-a-Number.py

--- 2520-Count-the-Digits-That-Divide-a-Number/2520-Count-the-Digits-That-Divide-a-Number.py
+++ 2520-Count-the-Digits-That-Divide-a-Number/2520-Count-the-Digits-That-Divide-a-Number.py
@@ -6,3 +6,8 @@ class Solution:
             if num % int(digit[i]) == 0:
                 count= count + 1
         return count
+    
+ ''' TC:  O(n), where n is the number of digits in the given number.
+ This is because the for loop iterates through all the digits in the number and performs a constant time operation for each iteration. '''
+
+''' SC: O(n), because we are creating a list of digits from the given number which takes up n space in memory. '''
