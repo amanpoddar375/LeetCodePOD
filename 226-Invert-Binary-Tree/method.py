df3eace0fede5a9e55c4c1b9e35f54cd29f6d1ed
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2023-02-18 23:13:55 +0545

    Create method.py

---
+++ 226-Invert-Binary-Tree/method.py
@@ -0,0 +1,8 @@
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        root.left, root.right = root.right, root.left
+        return root
