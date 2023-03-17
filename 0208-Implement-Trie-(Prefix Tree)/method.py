10a3476966fb2da9c2297b5d839231bd22d82405
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2023-03-17 19:50:40 +0545

    Updated method.py

---
+++ 0208-Implement-Trie-(Prefix Tree)/method.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+
+'''# TC of insert, search, and startsWith methods : O(m), where m is the length of the word or prefix being inserted, searched, or checked.'''
+
+'''# SC of Trie : O(nm), where n is the number of words inserted into the Trie and m is the length of the longest word.'''
