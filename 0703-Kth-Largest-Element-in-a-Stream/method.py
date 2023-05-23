67bf6cb3cb5b29806ee7b114995fe7fb4d523232
amanpoddar375 <amanpoddar5566@gmail.com>
2023-05-23 22:15:29 +0545

    added python solution

---
+++ 0703-Kth-Largest-Element-in-a-Stream/method.py
@@ -0,0 +1,20 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = []
+        for num in nums:
+            self.add(num)
+    
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+# TC : O(n * log(k))
+# Sc : O(k)
