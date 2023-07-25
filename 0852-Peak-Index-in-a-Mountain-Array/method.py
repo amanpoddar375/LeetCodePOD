8d13c51eaf4b2f894dab1e2755aedbfd6bf9c6ce
amanpoddar375 <amanpoddar5566@gmail.com>
2023-07-25 16:12:10 +0545

    added python solution for peak index in the mountain array

---
+++ 0852-Peak-Index-in-a-Mountain-Array/method.py
@@ -0,0 +1,16 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        left, right = 0, len(arr) - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if arr[mid] < arr[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
+
+# TC : O(logn), where n is the length of arr
+# SC : O(1)
