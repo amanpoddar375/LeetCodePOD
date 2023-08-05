ed1e78eeede6923b9128d58f12f7f566e6ab5c31
amanpoddar375 <amanpoddar5566@gmail.com>
2023-08-05 22:28:56 +0545

    added python solution

---
+++ 0095-Unique-Binary-Search-Trees-II/method.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        def generate_trees(start, end):
+            if start > end:
+                return [None]
+
+            all_trees = []
+            for i in range(start, end + 1):
+                left_subtrees = generate_trees(start, i - 1)
+                right_subtrees = generate_trees(i + 1, end)
+
+                for left_tree in left_subtrees:
+                    for right_tree in right_subtrees:
+                        root = TreeNode(i)
+                        root.left = left_tree
+                        root.right = right_tree
+                        all_trees.append(root)
+
+            return all_trees
+
+        if n == 0:
+            return []
+        return generate_trees(1, n)
