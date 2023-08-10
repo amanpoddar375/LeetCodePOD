62e75d8c74eda36fb928b77b1aff8bda561d5dee
amanpoddar375 <amanpoddar5566@gmail.com>
2023-08-10 22:13:05 +0545

    added python solution

---
+++ 0081-Search-In-Rotated-Sorted-Array-II/method.py
@@ -0,0 +1,30 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return True
+
+            # if the left half is sorted
+            if nums[left] < nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # if the right half is sorted
+            elif nums[left] > nums[mid]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            # if elements at left and mid are same
+            else:
+                left += 1
+
+        return False
+
+# TC : O(log N), where N is the number of elements in the nums array.
+# SC : O(1)
