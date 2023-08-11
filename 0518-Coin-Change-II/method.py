6483a2eccd8e603dc1aa284481c46f2b09d2945d
amanpoddar375 <amanpoddar5566@gmail.com>
2023-08-11 16:48:32 +0545

    added python solution

---
+++ 0518-Coin-Change-II/method.py
@@ -0,0 +1,13 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] += dp[x - coin]
+        
+        return dp[amount]
+
+# TC : O(amount * len(coins))
+# SC : O(amount)
