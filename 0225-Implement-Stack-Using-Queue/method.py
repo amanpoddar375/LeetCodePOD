ca9bebdd9211327b62976171090390a2bbc91c3a
amanpoddar375 <amanpoddar5566@gmail.com>
2023-08-28 22:44:25 +0545

    added python solution

---
+++ 0225-Implement-Stack-Using-Queue/method.py
@@ -0,0 +1,31 @@
+class MyStack:
+
+    def __init__(self):
+        self.queue1 = deque()
+        self.queue2 = deque()
+
+    def push(self, x: int) -> None:
+        self.queue2.append(x)
+        while self.queue1:
+            self.queue2.append(self.queue1.popleft())
+        self.queue1, self.queue2 = self.queue2, self.queue1
+
+    def pop(self) -> int:
+        return self.queue1.popleft()
+
+    def top(self) -> int:
+        return self.queue1[0]
+
+    def empty(self) -> bool:
+        return not self.queue1
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
+
+# TC : O(n), where n is the number of n elements in queue. push: O(n) also pop,top, empty : O(1)
+# SC : O(n)
