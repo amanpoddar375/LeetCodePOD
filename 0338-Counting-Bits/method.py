8e2552c796ba92babd55a00a30119aaabe55c393
amanpoddar375 <amanpoddar5566@gmail.com>
2023-09-01 22:32:38 +0545

    with O(n) time complexity

---
+++ 0338-Counting-Bits/method.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n+1)
+        for i in range(1,n+1):
+            ans[i] = ans[i>>1] + (i&1)
+        return ans
+        
+# TC: O(n)
+# SC: O(n)
