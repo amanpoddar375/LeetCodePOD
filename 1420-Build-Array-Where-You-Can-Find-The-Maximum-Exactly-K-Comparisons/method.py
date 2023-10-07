0f400116ef3995e7ec2c90a830befd5f6242d803
amanpoddar375 <amanpoddar5566@gmail.com>
2023-10-07 23:59:01 +0545

    added python solution

---
+++ 1420-Build-Array-Where-You-Can-Find-The-Maximum-Exactly-K-Comparisons/method.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numOfArrays(self, n: int, m: int, k: int) -> int:
+
+        if m < k:
+            return 0
+        dp = [[1] * m] + [[0] * m for _ in range(k - 1)]
+        mod = 10 ** 9 + 7
+        
+        for _ in range(n - 1):
+            for i in range(k - 1, -1, -1):
+                cur = 0
+                for j in range(m):
+                    dp[i][j] = (dp[i][j] * (j + 1) + cur) % mod
+                    if i:
+                        cur = (cur + dp[i - 1][j]) % mod
+        
+        return sum(dp[-1]) % mod
