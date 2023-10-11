c0644d304eb4ddf9b469a5d750c467e8ab1c9b97
amanpoddar375 <amanpoddar5566@gmail.com>
2023-10-11 10:06:40 +0545

    updated python solution

--- 2009-Minimum-Number-of-Operations-to-Make-Array-Continuous/method.py
+++ 2009-Minimum-Number-of-Operations-to-Make-Array-Continuous/method.py
@@ -1,17 +1,14 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-
         unique_nums = sorted(set(nums))
-        n = len(unique_nums)
-        maxLen = 0
-        left = 0
-        
-        for right in range(n):
-            while unique_nums[right] - unique_nums[left] > n - 1:
-                left += 1
-            maxLen = max(maxLen, right - left + 1)
-        
-        return len(nums) - maxLen
+        n = len(nums)
+        result = n
+        for i, left in enumerate(unique_nums):
+            right = left + n -1
+            pos = bisect_right(unique_nums,right)
+            uniqueLen = pos - i
+            result = min(result, n - uniqueLen)
+        return result
 
 # TC : O(nlogn)
 # SC : O(n)
