4e948c7bedb4eef2acf0c7cbb5de24c8c8de6eb7
amanpoddar375 <amanpoddar5566@gmail.com>
2023-10-27 12:26:36 +0545

    added python solution

---
+++ 0005-Longest-Palindrome-Substring/method.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest_palindrome = ''
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        
+        for i in range(n):
+            dp[i][i] = True
+            longest_palindrome = s[i]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    if j - i == 1 or dp[i + 1][j - 1]:
+                        dp[i][j] = True
+                        if len(longest_palindrome) < len(s[i:j + 1]):
+                            longest_palindrome = s[i:j + 1]
+        
+        return longest_palindrome
+
+# TC : O(n^2), where 'n' is the length of the input string 's'.
+# SC : O(n^2)
