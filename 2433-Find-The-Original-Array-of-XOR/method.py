96fe974d4256e84d7a1873c6df7688a0b6b4b6c4
amanpoddar375 <amanpoddar5566@gmail.com>
2023-10-31 21:34:11 +0545

    added python solution

---
+++ 2433-Find-The-Original-Array-of-XOR/method.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        n = len(pref)
+        arr = [0] * n
+        arr[0] = pref[0]
+        for i in range(1, n):
+            arr[i] = pref[i] ^ pref[i - 1]
+        return arr
+
+# TC : O(n), where "n" is the size of the input pref array.
+# SC : O(n).
