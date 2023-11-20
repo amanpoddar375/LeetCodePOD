d02fe8f1650c8e0d7ebd4d0e506c25290e34fcae
amanpoddar375 <amanpoddar5566@gmail.com>
2023-11-20 15:35:45 +0545

    added python solution

--- 2391-Minimum-Amount-of-Time-to-Collect-Garbage/method.py
+++ 2391-Minimum-Amount-of-Time-to-Collect-Garbage/method.py
@@ -1,6 +1,5 @@
 class Solution:
-    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
-
-        last = {c: i for i, pgm in enumerate(A) for c in pgm}
-        dis = list(accumulate(travel, initial=0))
-        return len(''.join(A)) + sum(dis[last.get(c, 0)] for c in 'PGM')
+    def garbageCollection(self, A: List[str], travel: List[int]) -> int:
+        last = {c: i for i,pgm in enumerate(A) for c in pgm}
+        dis = list(accumulate(travel, initial = 0))
+        return sum(map(len, A)) + sum(dis[last.get(c, 0)] for c in 'PGM')
