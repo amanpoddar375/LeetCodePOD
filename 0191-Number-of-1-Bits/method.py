6cb34312a8cc607c001ff27eb89acac44ffc7dff
amanpoddar375 <amanpoddar5566@gmail.com>
2023-11-29 15:48:28 +0545

    added python solution

---
+++ 0191-Number-of-1-Bits/method.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            count += n & 1
+            n >>= 1
+        return count
+
+# TC : O(logN)
+# SC : O(1)
